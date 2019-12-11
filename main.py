0aa151d40ef31dec435d310ade814c5f314ae7e1
MaowMan <54308739+MaowMan@users.noreply.github.com>
2019-12-11 23:00:54 +0800

    mid

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from tkinter import *
 import requests
 import tkinter.font as tkFont
-
+from bs4 import BeautifulSoup
 class Main(object):
     def __init__(self):
         self.Setup()
@@ -11,7 +11,6 @@ class Main(object):
         self.Root=Tk()
         self.Root.title("感測器控制平台")
         #self.Root.geometry("250x250")
-        #self.Root.resizable(0,0)
         self.tempVar=StringVar()
         self.tempVar.set("offline")
         self.humiVar=StringVar()
@@ -58,27 +57,51 @@ class Main(object):
         self.Buttons[-1].grid(row=1,column=1,columnspan=1,sticky=W+E)
         self.Labels.append(Label(self.Root,text="RGB控制",font=self.Font,fg="blue",relief="solid"))
         self.Labels[-1].grid(row=5,column=0,columnspan=3,sticky=W+E)
-        self.Scales.append(Scale(self.Root,variable=self.rVar,command=self.updatergb,from_=0,to=255,orient=HORIZONTAL,relief="solid",label="紅："))
+        self.Scales.append(Scale(self.Root,variable=self.rVar,command=lambda x : self.updatergb(1,self.rVar.get()),from_=0,to=255,orient=HORIZONTAL,relief="solid",label="紅："))
         self.Scales[-1].grid(row=6,column=0,columnspan=3,sticky=W+E)
-        self.Scales.append(Scale(self.Root,variable=self.gVar,command=self.updatergb,from_=0,to=255,orient=HORIZONTAL,relief="solid",label="綠："))
+        self.Scales.append(Scale(self.Root,variable=self.gVar,command=lambda x : self.updatergb(2,self.gVar.get()),from_=0,to=255,orient=HORIZONTAL,relief="solid",label="綠："))
         self.Scales[-1].grid(row=7,column=0,columnspan=3,sticky=W+E)
-        self.Scales.append(Scale(self.Root,variable=self.bVar,command=self.updatergb,from_=0,to=255,orient=HORIZONTAL,relief="solid",label="藍："))
+        self.Scales.append(Scale(self.Root,variable=self.bVar,command=lambda x : self.updatergb(3,self.bVar.get()),from_=0,to=255,orient=HORIZONTAL,relief="solid",label="藍："))
         self.Scales[-1].grid(row=8,column=0,columnspan=3,sticky=W+E)
+        self.Root.resizable(0,0)
     def Loop(self):
         self.Root.after(200,self.updateinfo)
         self.Root.mainloop()
     def updateinfo(self):
         print("updateinfo")
+        respond=requests.get("http://192.168.4.1/value.html")
+        bs4=BeautifulSoup(respond.text,"lxml")
+        
+        self.tempVar.set(bst.find)
         self.Root.after(200,self.updateinfo)
     def onmain(self):
         print("onmain")
+        try:
+            requests.get("http://192.168.4.1/r1on.html")
+        except(requests.exceptions.ConnectionError):
+            pass
     def offmain(self):
         print("offmain")
+        try:
+            requests.get("http://192.168.4.1/r1off.html")
+        except(requests.exceptions.ConnectionError):
+            pass
     def onbuzz(self):
         print("onbuzz")
+        try:
+            requests.get("http://192.168.4.1/r2on.html")
+        except(requests.exceptions.ConnectionError):
+            pass
     def offbuzz(self):
         print("offbuzz")
-    def updatergb(self,*args):
-        print("{},{},{}".format(self.rVar.get(),self.gVar.get(),self.bVar.get()))
+        try:
+            requests.get("http://192.168.4.1/r2off.html")
+        except(requests.exceptions.ConnectionError):
+            pass
+    def updatergb(self,pin,value):
+        try:
+            requests.get("http://192.168.4.1/color{}={}".format(pin,value))
+        except(requests.exceptions.ConnectionError):
+            pass
 if __name__=="__main__":
     Main()
